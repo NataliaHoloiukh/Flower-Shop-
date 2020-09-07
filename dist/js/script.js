389973df2ad1cec54862434cf817a03e8b435819
unknown <revolveng91@gmail.com>
2020-09-08 01:25:29 +0300

    all

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,13 +1,24 @@
 new Splide( '.splide', {
     type: 'loop',
     speed: 2000,
-    autoplay: false,
+    autoplay: true,
     perPage: 2,
     perMove: 2,
-    gap: '1em',
+    gap: '1.2em',
     pagination: true,
     arrows: false
 } ).mount();
+
+new Splide( '.splideSmall', {
+    type: 'loop',
+    speed: 2000,
+    autoplay: true,
+    perPage: 1,
+    perMove: 1,
+    gap: '1em',
+    pagination: true,
+    arrows: false
+} ).mount();;
 function initMap() {
     let ny = {lat: 46.477524, lng: 30.739699};
     let center = {lat: 46.476100, lng: 30.739699};
@@ -187,3 +198,87 @@ function initMap() {
         map: map
     });
 }
+;
+
+
+
+
+// See More button
+let itemMob = document.getElementsByClassName('hide');
+let seeButton = document.getElementById('seeButton');
+
+if(innerWidth <= 480){
+    for (let n = 0; n < itemMob.length; n++){
+        itemMob[n].style.display = "none";
+    }
+}
+
+function seeMore() {
+    for (let i = 0; i < itemMob.length; i++) {
+
+        if (itemMob[i].style.display === "none") {
+            itemMob[i].style.display = 'flex';
+            seeButton.innerHTML = 'See less';
+        } else {
+            itemMob[i].style.display = 'none';
+            seeButton.innerHTML = 'See more';
+        }
+    }
+}
+
+// Mobile Menu
+
+let x = document.getElementById('mobileMenu');
+let burg = document.getElementById('burg');
+x.style.display = "none";
+
+function mobileMenu() {
+    
+    burg.classList.toggle("change");
+    
+
+    if (x.style.display === 'flex') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'flex'}
+  }
+
+
+
+
+// Pop-up Order Bouquet
+function getContacts() {
+
+    let namaVal =  document.forms["forma"].elements["form-client"].value;
+    let telVal =  document.forms["forma"].elements["form-tel"].value;
+
+    console.log(`Client's name: ${namaVal}, User's tel: ${telVal}`);
+
+    let mess = document.createElement('p');
+    mess.className = 'message';
+    mess.id = 'message';
+    mess.innerHTML = 'Thank You for your message. We will get back to you soon =)';
+    let frm = document.getElementById('forma');
+    frm.append(mess);
+
+    setTimeout (function () {
+        document.getElementById('message').remove();
+    }, 5000)
+}
+
+
+let modal = document.getElementById('popUp');
+let wind = document.getElementById('modalW');
+let butn = document.getElementById('butOpenModal');
+modal.style.display = "none";
+
+function openModal(){
+    modal.style.display = 'flex';
+    wind.style.display = 'flex';
+}
+
+function closeModal(){
+    modal.style.display = 'none';
+    wind.style.display = 'none';
+}
+
--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,85 @@
-@@include('_slider.js')
-@@include('_map.js')
+@@include('_slider.js');
+@@include('_map.js');
+
+
+
+
+// See More button
+let itemMob = document.getElementsByClassName('hide');
+let seeButton = document.getElementById('seeButton');
+
+if(innerWidth <= 480){
+    for (let n = 0; n < itemMob.length; n++){
+        itemMob[n].style.display = "none";
+    }
+}
+
+function seeMore() {
+    for (let i = 0; i < itemMob.length; i++) {
+
+        if (itemMob[i].style.display === "none") {
+            itemMob[i].style.display = 'flex';
+            seeButton.innerHTML = 'See less';
+        } else {
+            itemMob[i].style.display = 'none';
+            seeButton.innerHTML = 'See more';
+        }
+    }
+}
+
+// Mobile Menu
+
+let x = document.getElementById('mobileMenu');
+let burg = document.getElementById('burg');
+x.style.display = "none";
+
+function mobileMenu() {
+    
+    burg.classList.toggle("change");
+    
+
+    if (x.style.display === 'flex') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'flex'}
+  }
+
+
+
+
+// Pop-up Order Bouquet
+function getContacts() {
+
+    let namaVal =  document.forms["forma"].elements["form-client"].value;
+    let telVal =  document.forms["forma"].elements["form-tel"].value;
+
+    console.log(`Client's name: ${namaVal}, User's tel: ${telVal}`);
+
+    let mess = document.createElement('p');
+    mess.className = 'message';
+    mess.id = 'message';
+    mess.innerHTML = 'Thank You for your message. We will get back to you soon =)';
+    let frm = document.getElementById('forma');
+    frm.append(mess);
+
+    setTimeout (function () {
+        document.getElementById('message').remove();
+    }, 5000)
+}
+
+
+let modal = document.getElementById('popUp');
+let wind = document.getElementById('modalW');
+let butn = document.getElementById('butOpenModal');
+modal.style.display = "none";
+
+function openModal(){
+    modal.style.display = 'flex';
+    wind.style.display = 'flex';
+}
+
+function closeModal(){
+    modal.style.display = 'none';
+    wind.style.display = 'none';
+}
+
